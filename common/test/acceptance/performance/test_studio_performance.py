e72d3485ab1b3deed0a2536a941dcdda3d6ab363
Daniel Friedman <dfriedman58@gmail.com>
2014-07-23 17:46:15 -0400

    Refactor into course-agnostic helper methods

--- common/test/acceptance/performance/test_studio_performance.py
+++ common/test/acceptance/performance/test_studio_performance.py
@@ -13,6 +13,81 @@ class StudioPagePerformanceTest(WebAppPerfReport):
     """
     Example test case.
     """
+    def setUp(self):
+        """
+        Authenticate as staff so we can view and edit courses.
+        """
+        super(StudioPagePerformanceTest, self).setUp()
+        AutoAuthPage(self.browser, staff=True).visit()
+
+    def record_visit_course_outline(self, course_outline_page):
+        """
+        Produce a performance report for visiting the course outline page.
+        """
+        har_name = 'OutlinePage_{org}_{course}'.format(
+            org=course_outline_page.course_info['course_org'],
+            course=course_outline_page.course_info['course_num']
+        )
+        self.new_page(har_name)
+        course_outline_page.visit()
+        self.save_har(har_name)
+
+    def record_visit_unit_page(self, course_outline_unit, course_info):
+        """
+        Produce a performance report for visiting a unit page.
+        """
+        har_name = 'UnitPage_{org}_{course}'.format(
+            org=course_info['course_org'],
+            course=course_info['course_num']
+        )
+        self.new_page(har_name)
+        course_outline_unit.go_to()
+        self.save_har(har_name)
+
+    def record_update_subsection_in_course_outline(self, course_outline_page, section_title, original_subsection_title, with_cache):
+        """
+        Produce a performance report for updating a subsection on the
+        outline page.
+        """
+        edited_subsection_title = "Edited Subsection Title"
+
+        # Since this method is called twice, the subsection we want
+        # will either have its original name or our edited one.
+        if with_cache:
+            subsection = course_outline_page.section(section_title).subsection(edited_subsection_title)
+        else:
+            subsection = course_outline_page.section(section_title).subsection(original_subsection_title)
+
+        har_name = 'OutlinePageUpdateSubsection_{org}_{course}'.format(
+            org=course_outline_page.course_info['course_org'],
+            course=course_outline_page.course_info['course_num']
+        )
+        self.new_page(har_name)
+        if with_cache:
+            subsection.change_name(original_subsection_title)
+        else:
+            subsection.change_name(edited_subsection_title)
+        self.save_har(har_name)
+
+    def record_publish_unit_page(self, course_outline_unit, course_info):
+        """
+        Produce a performance report for publishing an edited unit container page.
+        """
+        locator = course_outline_unit.locator
+        course_outline_unit.go_to()
+
+        container_page = ContainerPage(self.browser, locator)
+        container_page.delete(0)
+
+        har_name = 'UnitPagePublish_{org}_{course}'.format(
+            org=course_info['course_org'],
+            course=course_info['course_num']
+        )
+        self.new_page(har_name)
+        # TODO: make the below two steps a method in ContainerPage
+        container_page.publish_action.click()
+        container_page.wait_for_ajax()
+        self.save_har(har_name)
 
     @with_cache
     def test_signup_flow_with_cache(self):
@@ -67,55 +142,32 @@ class StudioPagePerformanceTest(WebAppPerfReport):
         self.save_har('SignupPage')
 
     @with_cache
-    def test_visit_course_outline_with_cache(self):
+    def test_justice_visit_outline(self):
         """
         Produce a report for the outline page performance.
         """
-        auth_page = AutoAuthPage(self.browser, staff=True)
-        auth_page.visit()
-        course_outline_page = CourseOutlinePage(self.browser, 'HarvardX', 'ER22x', '2013_Spring')
-
-        self.new_page('OutlinePage')
-        course_outline_page.visit()
-        self.save_har('OutlinePage')
+        self.record_visit_course_outline(CourseOutlinePage(self.browser, 'HarvardX', 'ER22x', '2013_Spring'))
 
     @with_cache
-    def test_update_subsection_in_course_outline_with_cache(self):
+    def test_justice_update_subsection(self):
         """
-        Produce a report for the performance of updating a subsection
-        on the outline page.
+        Record updating a subsection on the Justice outline page.
         """
-        auth_page = AutoAuthPage(self.browser, staff=True)
-        auth_page.visit()
         course_outline_page = CourseOutlinePage(self.browser, 'HarvardX', 'ER22x', '2013_Spring')
         course_outline_page.visit()
 
-        section_title = 'Lecture 1 - Doing the Right Thing'
-        initial_subsection_title = 'Discussion Prompt: Ethics of Torture'
-        edited_subsection_title = 'New Subsection Title'
-
-        if self.with_cache:
-            subsection = course_outline_page.section(section_title).subsection(edited_subsection_title)
-        else:
-            subsection = course_outline_page.section(section_title).subsection(initial_subsection_title)
-
-        self.new_page('OutlinePageUpdateSubsection')
-        # Since this test is run twice, (second time cached), we need
-        # to have a different name each time, otherwise the edit won't
-        # go through
-        if self.with_cache:
-            subsection.change_name(initial_subsection_title)
-        else:
-            subsection.change_name(edited_subsection_title)
-        self.save_har('OutlinePageUpdateSubsection')
+        self.record_update_subsection_in_course_outline(
+            course_outline_page,
+            'Lecture 1 - Doing the Right Thing',
+            'Discussion Prompt: Ethics of Torture',
+            self.with_cache
+        )
 
     @with_cache
-    def test_visit_unit_page_with_cache(self):
+    def test_justice_visit_unit_page(self):
         """
         Produce a report for the unit page performance.
         """
-        auth_page = AutoAuthPage(self.browser, staff=True)
-        auth_page.visit()
         course_outline_page = CourseOutlinePage(self.browser, 'HarvardX', 'ER22x', '2013_Spring')
         course_outline_page.visit()
 
@@ -123,33 +175,20 @@ class StudioPagePerformanceTest(WebAppPerfReport):
         subsection_title = 'Discussion Prompt: Ethics of Torture'
         unit_title = subsection_title
 
-        unit = course_outline_page.section(section_title).subsection(subsection_title).toggle_expand().unit(unit_title)
-
-        self.new_page('UnitPage')
-        unit.go_to()
-        self.save_har('UnitPage')
+        course_outline_unit = course_outline_page.section(section_title).subsection(subsection_title).toggle_expand().unit(unit_title)
+        self.record_visit_unit_page(course_outline_unit, course_outline_page.course_info)
 
     @with_cache
-    def test_publish_unit_page_with_cache(self):
+    def test_justice_publish_unit_page(self):
         """
         Produce a report for the performance of publishing a unit with changes.
         """
-        auth_page = AutoAuthPage(self.browser, staff=True)
-        auth_page.visit()
         course_outline_page = CourseOutlinePage(self.browser, 'HarvardX', 'ER22x', '2013_Spring')
         course_outline_page.visit()
 
         section_title = 'Lecture 1 - Doing the Right Thing'
         subsection_title = 'Discussion Prompt: Ethics of Torture'
         unit_title = subsection_title
-        unit = course_outline_page.section(section_title).subsection(subsection_title).toggle_expand().unit(unit_title)
-        locator = unit.locator
-        unit.go_to()
+        course_outline_unit = course_outline_page.section(section_title).subsection(subsection_title).toggle_expand().unit(unit_title)
 
-        container_page = ContainerPage(self.browser, locator)
-        container_page.delete(0)
-
-        self.new_page('PublishUnitPage')
-        container_page.publish_action.click()
-        container_page.wait_for_ajax()
-        self.save_har('PublishUnitPage')
+        self.record_publish_unit_page(course_outline_unit, course_outline_page.course_info)
